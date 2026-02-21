3d9f38f9195d5024e2c4cbd22c28bc846d80ee2b
Harshita-008 <harshitahirawat08@gmail.com>
2026-02-21 18:08:21 +0530

    added frontend url

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://clearpath-chatbot.onrender.com"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
